6776e14d954550752b450c9fbc3894bb56afd759
Punna <punnarao14@gmail.com>
2021-06-11 19:49:44 +0530

    Bus tickets

---
+++ src/main/java/BUS-Tickets/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package BUS-Tickets;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author krieger
+ */
+@ApplicationPath("rest")
+public class JAXRSConfiguration extends Application {
+    
+}
